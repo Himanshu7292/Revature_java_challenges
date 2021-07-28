37d81e3abe15708e1de45dd5e441b21b8083a6b3
unknown <hrj7292@gmial.com>
2021-07-28 15:48:40 +0530

    regex push

---
+++ regexpractice/First_Regex.java
@@ -0,0 +1,15 @@
+package regexpractice;
+
+import java.lang.reflect.Array;
+import java.util.Arrays;
+
+public class First_Regex {
+public static void main(String[] args) {
+	/* */
+	String s = "fefsf^&(*$$%@s";
+ char c[]= s.toCharArray();
+ Arrays.sort(c);
+	System.out.println(c);
+}
+
+}
